3a9f4fc09d0108d9f6b9c72ce2d17570ad16b632
Sasindu Kaveesha Gamapathiarana <gsasindukaveesha@gmail.com>
2026-03-16 11:19:29 +0530

    Update AudioBook.java
    corrected a misspelled comment

--- src/AudioBook.java
+++ src/AudioBook.java
@@ -2,7 +2,7 @@ public class AudioBook extends Book {
     private int runTime;
 
     AudioBook(String title, String author, int runTime) {
-        super(title, author, 0); // super is reference to the parentclas of our current class
+        super(title, author, 0); // super is reference to the parent class of our current class
 
         this.runTime = runTime;
     } 
